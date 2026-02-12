4a38d0c4a01e56c20bb01c0b07c6b07b9908a980
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-02-13 00:22:42 +0600

    error handled

--- js/script.js
+++ js/script.js
@@ -17,14 +17,24 @@ const displayLessonWords = (words) => {
   const wordsContainer = document.getElementById("words-container");
   wordsContainer.innerHTML = "";
 
+  if(words == 0){
+    wordsContainer.innerHTML =
+    `
+    <div class="col-span-full text-center">
+            <img class="mx-auto" src="./assets/alert-error.png" alt="alert">
+            <p class="bangla-font text-md text-gray-400 font-medium my-4">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="bangla-font text-4xl font-bold">নেক্সট Lesson এ যান</h2>
+        </div>
+    `
+  }
+
   words.map((word) => {
-    console.log(word)
     const card = document.createElement("div");
     card.innerHTML = `
     <div class="bg-white rounded-xl shadow-sm text-center flex flex-col justify-evenly space-y-4 p-5">
-            <h2 class="font-bold text-2xl">${word.word}</h2>
-            <p class="font-semibold">Meaning / Pronounciation</p>
-            <div class="font-medium text-2xl bangla-font">${word.meaning} / ${word.pronunciation}</div>
+            <h2 class="font-bold text-2xl">${word.word? word.word : `<span class="bg-red-500">শব্দ পাওয়া যায়নি</span>`}</h2>
+            <p class="font-semibold">Meaning / Pronunciation</p>
+            <div class="font-medium text-2xl bangla-font">${word.meaning? word.meaning : `<span class="bg-red-500">অর্থ পাওয়া যায়নি</span>`} / ${word.pronunciation? word.pronunciation: `<span class="bg-red-500">উচ্চারণ পাওয়া যায়নি</span>` }</div>
             <div class="flex justify-between items-center">
                 <button class="btn bg-blue-100 hover:bg-blue-300"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-blue-100 hover:bg-blue-300"><i class="fa-solid fa-volume-high"></i></button>
